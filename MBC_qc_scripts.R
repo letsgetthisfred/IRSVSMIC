834bec0c133d13142ddf5fa32aec1021cc35d1c1
Fred Heller <fjh@duke.edu>
2020-09-17 17:00:35 -0700

    further work

--- MBC_qc_scripts.R
+++ MBC_qc_scripts.R
@@ -2,21 +2,22 @@
 #want a stim to unstim ratio of >=4 (taken from Falkard 2019), and KLH =<3
 
 
-check_ratio<-function(stim, unstim){
-        pass<-matrix(0,0,2)
-        fail<-matrix(0,0,2)
+check_ratio<-function(stim, unstim){ #this feels inelegant,but perhaps serviceable. would have to do x3 unless can create matrix where "pass" values added by index
+        pass<-data.frame(0,0,3)
+        colnames(pass)<-c("SID","Total Stim:Total Unstim", "KLH")
+        fail<-data.frame(0,0,2)
+        colnames(fail)<-c("SID","Total Stim:Total Unstim", "KLH")
         for(i in len(unstim)){
-                if(stim[i,"total"]/unstim[i,"total"]>=4){
-                        pass<-c(i, stim[i,"total"]/unstim[i,"total"])
+                if((stim[i,"total"]/unstim[i,"total"]>=4) & (stim[i,"KLH"]<=3])){
+                        pass<-rbind.data.frame(unstim[i,"SID"], stim[i,"total"]/unstim[i,"total"], stim[i,"KLH"])
                 }
                 else{
-                        fail<-c(unstim[i,"SID"], stim[i,"total"]/unstim[i,"total"])    
+                        fail<-rbind.data.frame(unstim[i,"SID"], stim[i,"total"]/unstim[i,"total"], stim[i,"KLH"])    
                 }
         }
 
         return(pass)
 }
 
-check_KLH<-function()
-        
+
 MBC_chec<-fun(data,)
